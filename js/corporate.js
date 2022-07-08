38e1a38a5f6f12b0e88ca57d58d2d75b89ef7e56
yamone178 <yamonemyatmoe1592@gmail.com>
2022-07-08 12:49:11 +0630

    Revert "to-revert-2"
    This reverts commit 89dc81af5d12e23998d809e563f85a5a191b7f56.

--- js/corporate.js
+++ js/corporate.js
@@ -1,4 +1,4 @@
-import '../scss/style.scss'
+import '../style.scss'
 import Typed from 'typed.js';
 
 
--- js/services.js
+++ js/services.js
@@ -1,5 +1,5 @@
-import '../scss/style.scss'
-import '../scss/services.scss'
+import '../style.scss'
+import '../services.scss'
 
 import Typed from 'typed.js';
 
