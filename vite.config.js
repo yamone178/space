75a05d9204d2bff965ac95192ffacf49ae2245ba
yamone178 <yamonemyatmoe1592@gmail.com>
2022-07-13 17:46:20 +0630

    Revert "meta-tag"
    This reverts commit 01afcb1c1d3d4e09597f8f508e4ac4ef01b6efd7.

--- vite.config.js
+++ vite.config.js
@@ -27,37 +27,7 @@ module.exports = {
     }
 }
 
-module.exports = {
-    // remove modules you don't need
-    modules: [
-        'virtual',
-        'keyboard',
-        'mousewheel',
-        'navigation',
-        'pagination',
-        'scrollbar',
-        'parallax',
-        'zoom',
-        'lazy',
-        'controller',
-        'a11y',
-        'history',
-        'hash-navigation',
-        'autoplay',
-        'thumbs',
-        'free-mode',
-        'grid',
-        'manipulation',
-        'effect-fade',
-        'effect-cube',
-        'effect-flip',
-        'effect-coverflow',
-        'effect-creative',
-        'effect-cards',
-    ],
-};
 
-module.exports = {
-    devtool: "source-map",
-}
+
+
 
